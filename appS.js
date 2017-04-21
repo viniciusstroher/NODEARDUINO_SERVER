66e3fe2ed325be7e79567657885886f358c49242
vinicius~~ <viniciusferreirwk@gmail.com>
2017-04-21 18:11:01 -0300

    fix

--- appS.js
+++ appS.js
@@ -1,17 +1,17 @@
 function startServer(){
 	try{
 		var net = require('net');
-		console.log("[Log]iniciando servidor");
+		console.log("[Log - "+new Date().toISOString()+"]iniciando servidor");
 		
 
 		var server = net.createServer(function(socket) {
-			console.log("[Log '+new date().toisostring()+']Cliente Conectado");
+			console.log("[Log - "+new Date().toISOString()+"]Cliente Conectado");
 			
-			socket.write('[Log '+new date().toisostring()+']CONNECTION ACEPTED\r\n');
+			socket.write('[Log - '+new Date().toISOString()+']CONNECTION ACEPTED\r\n');
 	
 			// Handleflg incoming messages from clients.
 			socket.on('data', function (data) {
-				var log = '[Log '+new date().toisostring()+'] Data Socket: '+data.toString('utf8');
+				var log = '[Log - '+new Date().toISOString()+'] Data Socket: '+data.toString('utf8');
 			    console.log(log);
 			    try{
 				    /*var param = data.toString('utf8').split("#");
