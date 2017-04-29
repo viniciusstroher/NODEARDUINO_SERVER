3b8b14532111bacd8b75ff7d185beb52d4e2a6fc
vinicius~~ <viniciusferreirwk@gmail.com>
2017-04-29 15:08:11 -0300

    fix

--- appS.js
+++ appS.js
@@ -2,6 +2,7 @@ function startServer(){
 	try{
 		var net = require('net');
 		var fs  = require('fs');
+		var jsonData = null;
 
 		console.log("[Log - "+new Date().toISOString()+"]Iniciando servidor.\r\n");	
 
@@ -11,6 +12,12 @@ function startServer(){
 			socket.write(connect_log);
 	
 			socket.on('data', function (data) {
+				try{
+					jsonData = JSON.parse(data.toString('utf8'));
+				}catch(ex){
+
+				}
+
 				var log = '[Log - '+new Date().toISOString()+'] \n ARDUINO DATA: '+data.toString('utf8')+"\r\n";
 			    console.log(log);
 
@@ -22,8 +29,8 @@ function startServer(){
 					    // done
 					  }
 					});
-			    
-
+			    	
+					console.log(jsonData);
 				    /*var param = data.toString('utf8').split("#");
 				    var value = param[3].split(":");
 
@@ -37,13 +44,43 @@ function startServer(){
 				    }*/
 				}catch(ex){
 					console.log("ERROR SOCKET DATA");
+					socket.destroy();
 				}
 			});
-
+			socket.on('error', function (data) {
+				console.log('error socket',data);
+				
+			});
 			//socket.pipe(socket);
 		});
 
 		server.listen(8090);
+
+		var app = require('http').createServer(handler)
+		var io = require('socket.io')(app);
+		
+		app.listen(8091);
+
+		function handler (req, res) {
+		  fs.readFile(__dirname + '/index.html',
+		  function (err, data) {
+		    if (err) {
+		      res.writeHead(500);
+		      return res.end('Error loading index.html');
+		    }
+
+		    res.writeHead(200);
+		    res.end(data);
+		  });
+		}
+
+		io.on('connection', function (socket) {
+		  socket.emit('news', { hello: 'world' });
+		  socket.on('my other event', function (data) {
+		    console.log(data);
+		  });
+		});
+		
 	}catch(ex){
 		console.log("RESTART SERVER");
 		startServer();
