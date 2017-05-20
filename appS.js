b08effc1a88ba709e7a768fb7b3e4dff5468686b
vinicius~~ <viniciusferreirwk@gmail.com>
2017-05-20 17:44:32 -0300

    fix

--- appS.js
+++ appS.js
@@ -142,9 +142,12 @@ function startServer(){
 
 					jsonData = jsonDataAux;
 					//FATOR DE CORRECAO
-					jsonData.luminosidade  = jsonData.luminosidade  * ldr1_fator; 
-					jsonData.luminosidade2 = jsonData.luminosidade2 * ldr2_fator; 
+					console.log(jsonData.luminosidade,'@',ldr1_fator,'@',jsonData.luminosidade*ldr1_fator);
+					console.log(jsonData.luminosidade2,'@',ldr2_fator,'@',jsonData.luminosidade2*ldr2_fator);
 
+					jsonData.luminosidade  = parseFloat(jsonData.luminosidade)  * ldr1_fator; 
+					jsonData.luminosidade2 = parseFloat(jsonData.luminosidade2) * ldr2_fator; 
+					
 					pir_1 = jsonData.movimentacao;
 					pir_2 = jsonData.movimentacao2;
 
